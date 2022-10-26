ff15e680a090149019ac378b81d6b666d342f0ae
louragini <medlouragini@gmail.com>
2022-10-26 19:07:15 +0100

    Variables

---
+++ Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables {
+
+    public static void main(String[] args){
+
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "ENISo";
+
+        System.out.println("Hello "+name);
+
+    }
+}
